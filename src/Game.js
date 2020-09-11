1ca3cfc4fb88594f2864a7aadbefa1c94b46692d
Veaceslav Barbarii <veaceslav.barbarii@vizolution.co.uk>
2020-09-11 13:00:40 +0300

    P-0 Added volatility list items identity

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Board } from './Board';
 import { calculateWinner } from "./calculateWinner";
+// import { v4 as uuid } from 'uuid';
 
 export class Game extends React.Component {
 
   constructor(props) {
     super(props);
 
+    const initialLastIdentityIndex = 1;
+
     this.state = {
       history: [{
-        squares: new Array(9).fill(null)
+        squares: new Array(9).fill(null),
+        itemKey: initialLastIdentityIndex
       }],
       xIsNext: true,
-      winnerCells: []
+      winnerCells: [],
+      lastIdentityIndex: initialLastIdentityIndex
     };
   }
 
@@ -29,8 +34,17 @@ export class Game extends React.Component {
     }
 
     squares[squareIndex] = this.state.xIsNext ? 'X' : 'O';
+    const updatedIdentityIndex = this.state.lastIdentityIndex + 1;
+
+    const currentGameState = {
+      squares: squares,
+      itemKey: updatedIdentityIndex
+    };
+
+    this.setState({ lastIdentityIndex: updatedIdentityIndex })
+
     this.setState({
-      history: [...history, ...[ { squares: squares } ]],
+      history: [...history, ...[currentGameState]],
       xIsNext: !this.state.xIsNext
     });
 
@@ -49,11 +63,27 @@ export class Game extends React.Component {
     const current = history[history.length - 1];
     const gameResult = calculateWinner(current.squares);
 
-    let status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    console.log('----------------------- RE-RENDERING CYCLE ----------------------');
 
-    if (gameResult) {
-      status = `Winner: ${gameResult.winner}`;
-    }
+    const moves = history.map((step, moveIndex) => {
+
+      const desc = moveIndex
+        ? `Go to move ${moveIndex}`
+        : `Go to game start`;
+
+      console.log(`Move index: ${moveIndex}`);
+      console.log(`ID: ${step.itemKey}`);
+
+      return (
+        <li key={step.itemKey}>
+          <button onClick={() => this.jumpTo(moveIndex)}>{desc}</button>
+        </li>
+      );
+    })
+
+    let status = gameResult
+      ? `Winner: ${gameResult.winner}`
+      : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div className="game">
@@ -66,7 +96,7 @@ export class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol></ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
