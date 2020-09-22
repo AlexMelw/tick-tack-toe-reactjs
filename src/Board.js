d83cc2e68ef231e014e1f91d67757ac15f53bb2c
Veaceslav Barbarii <veaceslav.barbarii@vizolution.co.uk>
2020-09-22 10:28:02 +0300

    p-0 Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/Board.js
+++ src/Board.js
@@ -7,6 +7,7 @@ export class Board extends React.Component {
 
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         index={i}
         winnerCells={this.props.winnerCells}
@@ -16,26 +17,37 @@ export class Board extends React.Component {
 
   render() {
 
+    // let board = ``;
+
+    // for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+      
+      // for (let columnIndex = 0; columnIndex < 3; columnIndex++) {
+      //   this.renderSquare(3 * rowIndex + columnIndex)
+      // }
+
+      // const rowCells = [0, 1, 2].map(columnIndex => this.renderSquare(3 * rowIndex + columnIndex));
+
+      // board += `
+      //   <div className="board-row">
+      //     ${rowCells}
+      //   </div>
+      // `;
+    // }
+
+    const board = [0, 1, 2].map(rowIndex => {
+
+        const rowCells = [0, 1, 2].map(columnIndex => this.renderSquare(3 * rowIndex + columnIndex));
+
+        return (
+          <div key={rowIndex} className="board-row">
+            {rowCells}
+          </div>
+        )
+      });
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-
+        {board}
       </div>
     );
   }
